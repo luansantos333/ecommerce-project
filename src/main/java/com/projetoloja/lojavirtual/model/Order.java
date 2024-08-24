dd35215920827fdccc81532d6c74b2e9c819b654
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-08-24 18:24:45 -0300

    feat: add enity order

--- src/main/java/com/projetoloja/lojavirtual/model/Order.java
+++ src/main/java/com/projetoloja/lojavirtual/model/Order.java
@@ -1,4 +1,42 @@
 package com.projetoloja.lojavirtual.model;
 
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity (name = "tb_order")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private Instant moment;
+    private OrderStatus orderStatus;
+    @ManyToOne
+    @JoinColumn (name = "client_id")
+    private User client;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
 }
