ae12f8646c89313f028e63b0e4cce4fae10039af
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-08-25 09:16:27 -0300

    feat: add one-to-one relationship between Order and Payment entities

---
+++ src/main/java/com/projetoloja/lojavirtual/model/Payment.java
@@ -0,0 +1,52 @@
+package com.projetoloja.lojavirtual.model;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity (name = "tb_payment")
+public class Payment {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column (columnDefinition = "TIMESTAMP WITHOUT TIMEZONE")
+    private Instant moment;
+    @OneToOne
+    @MapsId
+    private Order order;
+
+
+    public Payment() {
+    }
+
+    public Payment(long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
