8dd3d92d0757b353a51b6d25b02b58d3e27ae450
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-01 19:45:03 -0300

    fix: change to interface

--- src/main/java/com/projetoloja/lojavirtual/repository/ProductRepository.java
+++ src/main/java/com/projetoloja/lojavirtual/repository/ProductRepository.java
@@ -1,12 +1,20 @@
 package com.projetoloja.lojavirtual.repository;
 
 import com.projetoloja.lojavirtual.model.Product;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class ProductRepository implements JpaRepository <Long, Product> {
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
+@Repository
+public interface ProductRepository extends JpaRepository <Product, Long> {
 
 
 
