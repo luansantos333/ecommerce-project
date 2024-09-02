80e49a4d62ee3a174395c1c5e5a9f5235231eaf8
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-02 09:00:58 -0300

    feat: add findAll method

--- src/main/java/com/projetoloja/lojavirtual/controllers/ProductController.java
+++ src/main/java/com/projetoloja/lojavirtual/controllers/ProductController.java
@@ -1,20 +1,41 @@
 package com.projetoloja.lojavirtual.controllers;
 
+import com.projetoloja.lojavirtual.dto.ProductDTO;
+import com.projetoloja.lojavirtual.model.Product;
+import com.projetoloja.lojavirtual.repository.ProductRepository;
+import com.projetoloja.lojavirtual.service.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping (value = "/products")
 public class ProductController {
 
-    @GetMapping
-    public String testeController () {
+    private final ProductService productService;
 
-        return "Olá mundo!";
 
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
 
+    @GetMapping (value = "/{id}")
+    public ProductDTO findById (@PathVariable (name = "id") Long id) {
+
+        return productService.findById(id);
+
+    }
+
+    @GetMapping
+    public List<ProductDTO> findAll () {
+
+        return productService.findAll();
+
+    }
 
 }
