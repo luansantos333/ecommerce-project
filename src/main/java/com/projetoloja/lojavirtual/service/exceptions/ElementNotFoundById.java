d88368801a2ed9ffd58e0f8fb92e35388003f4d8
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-03 09:21:54 -0300

    feat: add ElementNotFoundById exception class

---
+++ src/main/java/com/projetoloja/lojavirtual/service/exceptions/ElementNotFoundById.java
@@ -0,0 +1,12 @@
+package com.projetoloja.lojavirtual.service.exceptions;
+
+public class ElementNotFoundById extends Exception{
+
+    public ElementNotFoundById() {
+        super();
+    }
+
+    public ElementNotFoundById(String message) {
+        super(message);
+    }
+}
