325376f3022375745101dbece2e18e61e7e5d2b2
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-03 09:25:10 -0300

    fix: rename ElementNotFound exception class

---
+++ src/main/java/com/projetoloja/lojavirtual/service/exceptions/ElementNotFoundException.java
@@ -0,0 +1,12 @@
+package com.projetoloja.lojavirtual.service.exceptions;
+
+public class ElementNotFoundException extends Exception{
+
+    public ElementNotFoundException() {
+        super();
+    }
+
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+}
