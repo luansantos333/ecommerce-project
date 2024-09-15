2f4ec3592445d883ea37dc761f264c96cac22be7
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-15 10:51:21 -0300

    feat: add UserDetailsService class

---
+++ src/main/java/com/projetoloja/lojavirtual/service/UserService.java
@@ -0,0 +1,20 @@
+package com.projetoloja.lojavirtual.service;
+
+import com.projetoloja.lojavirtual.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
