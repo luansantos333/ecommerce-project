7f50258fe56a5cb099cfc3ce5f8270b94a8828fc
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-15 11:17:06 -0300

    feat: implment loadUserByUsername method

--- src/main/java/com/projetoloja/lojavirtual/service/UserService.java
+++ src/main/java/com/projetoloja/lojavirtual/service/UserService.java
@@ -1,9 +1,14 @@
 package com.projetoloja.lojavirtual.service;
 
+import com.projetoloja.lojavirtual.model.Role;
+import com.projetoloja.lojavirtual.model.User;
 import com.projetoloja.lojavirtual.repository.UserRepository;
+import com.projetoloja.lojavirtual.repository.projections.UserRoleProjection;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
 
 public class UserService implements UserDetailsService {
 
@@ -15,6 +20,29 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        List<UserRoleProjection> users = repository.searchUserRolesPasswordByEmail(username);
+
+        if (users.size() == 0) {
+
+
+            throw new UsernameNotFoundException("Could not found any user with the username informed");
+
+        }
+
+        User user = new User();
+        user.setEmail(users.get(0).getUsername());
+        user.setPassword(users.get(0).getPassword());
+
+
+        for (UserRoleProjection u : users) {
+
+
+            user.addRole(new Role(u.getRoleId(), u.getAuthority()));
+
+        }
+
+
+        return user;
+
     }
 }
