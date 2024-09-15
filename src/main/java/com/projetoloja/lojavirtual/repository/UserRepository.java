46bebd83f433f21b69a5deb95bd930904fd97ac2
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-15 11:20:29 -0300

    fix: fix query

--- src/main/java/com/projetoloja/lojavirtual/repository/UserRepository.java
+++ src/main/java/com/projetoloja/lojavirtual/repository/UserRepository.java
@@ -1,11 +1,20 @@
 package com.projetoloja.lojavirtual.repository;
 
+import com.projetoloja.lojavirtual.model.User;
+import com.projetoloja.lojavirtual.repository.projections.UserRoleProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository <User, Long> {
 
 
+   @Query(nativeQuery = true, value = "SELECT u.email AS username, u.password, roles.id AS roleId, roles.authority FROM tb_user AS u" +
+           " INNER JOIN tb_user_role AS user_role ON u.id = user_role.user_id INNER JOIN tb_roles AS roles ON user_role.role_id = roles.id WHERE u.email = :email")
+    List<UserRoleProjection> searchUserRolesPasswordByEmail(String email);
 
 
 }
