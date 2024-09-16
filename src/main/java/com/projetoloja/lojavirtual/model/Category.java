f00cb44d1ae51a9f6bb5bc6a3ffd317de359f0ca
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-16 18:12:10 -0300

    fix: change primitive type to var id to wrapper

--- src/main/java/com/projetoloja/lojavirtual/model/Category.java
+++ src/main/java/com/projetoloja/lojavirtual/model/Category.java
@@ -10,7 +10,7 @@ import java.util.Set;
 public class Category {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     @ManyToMany (mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
