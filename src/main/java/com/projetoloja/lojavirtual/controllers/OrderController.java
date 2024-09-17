1414c480a6e39e2b095a458d1b073244569b885c
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-17 09:38:09 -0300

    feat: add insertOrder method

--- src/main/java/com/projetoloja/lojavirtual/controllers/OrderController.java
+++ src/main/java/com/projetoloja/lojavirtual/controllers/OrderController.java
@@ -5,10 +5,11 @@ import com.projetoloja.lojavirtual.service.OrderService;
 import com.projetoloja.lojavirtual.service.exceptions.ElementNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/orders")
@@ -21,14 +22,20 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping(value = "/{id}")
-    public ResponseEntity<OrderDTO> findById(@Valid @PathVariable(name = "id") Long id) throws ElementNotFoundException {
-
+    public ResponseEntity<OrderDTO> findById(@PathVariable(name = "id") Long id) throws ElementNotFoundException {
         OrderDTO p = orderService.findById(id);
-
         return ResponseEntity.ok(p);
 
+    }
+
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @PostMapping
+    public ResponseEntity<OrderDTO> insertOrder (@Valid @RequestBody OrderDTO dto) {
+        OrderDTO o = orderService.saveOrder(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(o.getId()).toUri();
+        return ResponseEntity.created(uri).body(o);
     }
 
 
