6f81539c35983cdca3147d78fd519861b93e10d2
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-19 09:04:38 -0300

    add: application-dev.properties and import sql to target classes

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -76,4 +76,3 @@ INSERT INTO tb_payment (order_id, moment) VALUES (2, TIMESTAMP WITH TIME ZONE '2
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1,1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1,2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2,1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2,2);
